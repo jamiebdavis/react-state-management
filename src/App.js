e4fe5a4c29c348cbdaffbacb6dfa652f9e60492e
jamiebdavis <jamied1985@gmail.com>
2020-04-01 11:16:20 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import MovieList from "./components/MovieList";
 import { MovieProvider } from "./context/MovieContext";
 import Nav from "./components/Nav";
+import AddMovie from "./components/addMovie";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="App">
         <Nav />
         <MovieList />
+        <AddMovie />
       </div>
     </MovieProvider>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MovieContext } from "../context/MovieContext";
 
 const Nav = () => {
+  const [movies, setMovies] = useContext(MovieContext);
+
   return (
     <nav>
       <ul>
         <li>Home</li>
         <li>About</li>
+        <li>Number of Movies: {movies.length}</li>
       </ul>
     </nav>
   );
